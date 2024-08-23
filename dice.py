050b74bafce5d8c912ee7f72f39c4f4615ca5b2e
Zonatte <zebedee113@gmail.com>
2024-08-23 13:23:45 +1000

    Initial commit

--- dice.py
+++ dice.py
@@ -1,1 +1,21 @@
-# put your dice_roll() function in this file
+import random
+
+def dice_roll(dice_type):
+    if dice_type == 'coin':
+        return random.randint(1, 2)
+    elif dice_type == 'd4':
+        return random.randint(1, 4)
+    elif dice_type == 'd6':
+        return random.randint(1, 6)
+    elif dice_type == 'd8':
+        return random.randint(1, 8)
+    elif dice_type == 'd10':
+        return random.randint(1, 10)
+    elif dice_type == 'd20':
+        return random.randint(1, 20)
+    elif dice_type == 'd100':
+        return random.randint(1, 100)
+    else:
+        raise ValueError("Invalid dice type. Choose from 'coin', 'd4', 'd6', 'd8', 'd10', 'd20', 'd100'.")
+
+
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from pyscript import document  # webpage module links
-# your roll_dice function should be saved in a file named 'dice.py'
-# uncomment the next line when you have this prepared
-# import dice
+from pyscript import document
+from dice import dice_roll
+
+dice_type = "coin"
+
+def dice_num():
 
 
-# GLOBAL (script-wide) variable
-# this stores the selected face option from the drop-down list
-dice_type = "Coin"
+
+
 
 
 def select_face_option(event):
-    global dice_type  # use global var named dice_type
-    ...  # replace with your own code
-
+    global dice_type
+    dice_type = document.getElementById('dice-type').value
 
 def roll_all_dice(event):
-    global dice_type  # use global var named dice_type
-    ...  # replace with your own code
+    global dice_type
 
+    for r in ...:
+       results = dice_roll(dice_type)
 
 def clear_history(event):
-    # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
     document.querySelector("div#roll-history").innerHTML = ""
+
+
+
