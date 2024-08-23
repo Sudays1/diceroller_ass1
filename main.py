fac2892fa12aa2af92396c770caed87420d47e7b
Zonatte <zebedee113@gmail.com>
2024-08-23 13:31:02 +1000

    Initial commit

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ from pyscript import document
 from dice import dice_roll
 
 dice_type = "coin"
-
-def dice_num():
-
-
-
-
-
+dice_num = 1
 
 def select_face_option(event):
     global dice_type
     dice_type = document.getElementById('dice-type').value
 
-def roll_all_dice(event):
-    global dice_type
+def update_dice_num(event):
+    global dice_num
+    dice_num = int(document.getElementById('dice-num').value)
 
-    for r in ...:
-       results = dice_roll(dice_type)
+def roll_all_dice(event):
+    global dice_type, dice_num
+
+    update_dice_num(event)
+
+    results = ""
+    for _ in range(dice_num):
+        result = dice_roll(dice_type)
+        results += f"Rolling a {dice_type}: {result}\n"
+
+    history_div = document.querySelector("div#roll-history")
+    history_div.textContent = results
 
 def clear_history(event):
     document.querySelector("div#roll-history").innerHTML = ""
-
 
 
