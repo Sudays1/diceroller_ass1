373e83c44a77b47417f45dc532194764b8c1cce9
Meryam101 <mbr339.my@gmail.com>
2024-08-29 11:27:38 +1000

    Initial commit

--- main.py
+++ main.py
@@ -8,22 +8,21 @@ def select_face_option(event):
     global dice_type
     dice_type = document.getElementById('dice-type').value
 
-def update_dice_num(event):
+def update_dice_num():
     global dice_num
     dice_num = int(document.getElementById('dice-num').value)
 
 def roll_all_dice(event):
     global dice_type, dice_num
 
-    update_dice_num(event)
+    update_dice_num()
 
     results = ""
     for _ in range(dice_num):
         result = dice_roll(dice_type)
-        results += f"Rolling a {dice_type}: {result}\n"
+        results += f"Rolling a {dice_type}: {result}<br>"
 
-    history_div = document.querySelector("div#roll-history")
-    history_div.textContent = results
+    document.querySelector("div#roll-history").innerHTML += results
 
 def clear_history(event):
     document.querySelector("div#roll-history").innerHTML = ""
